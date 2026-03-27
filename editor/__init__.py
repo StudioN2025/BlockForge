8abfcd1090f1e9c39f2334ff086f9a37f377bd98
StudioN2025 <abobachebaksar@gmail.com>
2026-03-27 17:14:18 +0300

    Create __init__.py

---
+++ editor/__init__.py
@@ -0,0 +1,9 @@
+# editor/__init__.py
+"""
+Пакет для редактора Block3D Engine
+"""
+
+from .main_window import MainWindow
+from .blockly_bridge import BlocklyBridge
+
+__all__ = ['MainWindow', 'BlocklyBridge']
